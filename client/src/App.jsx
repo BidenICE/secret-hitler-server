ecbb2f5859ab648f3c57aa114a84560d6ec50b15
BidenICE <tristantogo52@gmail.com>
2025-07-23 22:35:34 -0400

    Initial commit with client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center text-white text-4xl">
+      Tailwind is working!
+    </div>
+  );
+}
